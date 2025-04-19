98bded2539b912d1e0122807d972f7d35a4c97eb
ardaparl <ptrlakarda@gmail.com>
2025-04-20 01:14:44 +0300

    ShadowX.py
    Lol

--- ShadowX.py
+++ ShadowX.py
@@ -1,3 +1,24 @@
+import os
+import sys
+
+def install_ufonet():
+    print("[*] UFONet kurulumu başlatılıyor...")
+
+    # UFONet'i klonla
+    os.system("git clone https://github.com/epsylon/ufonet.git")
+
+    # UFONet dizinine gir
+    os.chdir("ufonet")
+
+    # Gereksinim dosyasını yükle
+    os.system("pip install -r requirements.txt")
+
+    print("[+] UFONet kurulumu tamamlandı.")
+
+def ddos_attack():
+    print("[!] UFONet DDoS Saldırısı başlatılıyor...")
+    os.system("python3 /data/data/com.termux/files/home/ardaparl/ufonet/ufonet.py")
+
 def main():
     while True:
         os.system("clear")
@@ -15,6 +36,7 @@ def main():
 [3] SQL Injection Finder
 [4] Whois Lookup
 [5] DDoS (UFONet)
+[6] UFONet Kurulumunu Yap
 [0] Çıkış
 """)
         choice = input("Seçimini yap: ")
@@ -29,6 +51,8 @@ def main():
             whois_lookup()
         elif choice == "5":
             ddos_attack()
+        elif choice == "6":
+            install_ufonet()
         elif choice == "0":
             print("Görüşürüz gardaş!")
             break
@@ -36,3 +60,7 @@ def main():
             print("Geçerli bir seçenek gir!")
 
         input("\nDevam etmek için Enter'a bas...")
+
+# Ana fonksiyonu başlat
+if __name__ == "__main__":
+    main()
